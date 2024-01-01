907b4d85030d30a7b9a52721f26e590f590776ae
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-01 09:31:42 +0000

    Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,4 +30,47 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let someNumber = "33"
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// ********************************Operations********************************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue)
+
+// console.log("2+3 = ", 2+3)
+// console.log("2-3 = ", 2-3)
+// console.log("2*3 = ", 2*3)
+// console.log("2**3 = ", 2**3)
+// console.log("2/3 = ", 2/3)
+// console.log("2%3 = ", 2%3)
+
+let str1 = "Vande "
+let str2 = "Mataram"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log(1 + 2 + 3) //6
+// console.log(1 + 2 + "3") //33
+// console.log(1 + "2" + 3) //123
+// console.log("1" + 2 + 3) //123
+
+// console.log(+true) //1
+// console.log(true) //true
+// console.log(+"") //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
