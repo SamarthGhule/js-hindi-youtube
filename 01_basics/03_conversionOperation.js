68b79e0dc2d465e283d8132483441cce7d4adea8
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-01 06:51:39 +0000

    Done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = ""
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//Conversion to Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+// "" => number(0)
+
+let isLoggedIn = "Samarth"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+//Conversion to Boolean
+// 1 => true
+// 0 => false
+// "" => false
+// "Samarth" => true
+
+let someNumber = "33"
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
