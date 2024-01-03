86ed22d5877cb9d0d4d7cb2680e61da14c190e6d
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-03 08:47:34 +0000

    Done

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -22,4 +22,28 @@ let myDate = new Date()
 
 //Time Stamp for Quizzes/ Polls
 let myTimeStamp = Date.now() //Gives time in milliseconds from 1 Jan 1970
-console.log(myTimeStamp)
+// console.log(myTimeStamp)
+
+let myCreateDate = new Date("01-14-2024") //Gives time in milliseconds from 1 Jan 1970 to 01-14-2024
+// console.log(myCreateDate.getTime()) 
+
+//Gives time in seconds from 1 Jan 1970
+// console.log(Math.floor(myTimeStamp/1000))
+
+
+let newDate = new Date()
+// console.log(newDate)
+
+//Indexing of Months start from 0 (0 = January...)
+// console.log(newDate.getMonth()) //0
+
+//Indexing of Days start from 1 (Monday = 1) 
+// console.log(newDate.getDay()) //3
+
+//Used in real world as below
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
+
