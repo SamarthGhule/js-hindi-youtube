49b976b10bf5ad5c133d06b5a2c1ab370bedc74f
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-08 11:56:14 +0000

    Done

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,14 @@
+//if
+
+//Writing statements in 'if' without using curly braces
+//This is not a good practice and is also not reccomendable (Do not use such practice)
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");  
+
+//Output:
+// test
+// test2
+
+
