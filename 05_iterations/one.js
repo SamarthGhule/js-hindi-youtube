cef81422bd6110a3678ac1dcab43d6b0b3a967a6
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-09 06:39:53 +0000

    Done

---
+++ 05_iterations/one.js
@@ -0,0 +1,30 @@
+// for
+
+// for(let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length)
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+// }
+
+//Javascript does not have out of bounds exception but undefined in itself is a problematic thing
+
+
+//Break
+
+for (let i = 1; i <= 20; i++) {
+    const element = i;
+    if(element==5)
+    {
+     console.log(`Detected 5`)
+     break
+    }
+
+    console.log(`Value of i is ${i}`)
+}
