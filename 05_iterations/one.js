5b11a517c46e85498b8ccc6fd701b633f0affc37
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-09 06:41:12 +0000

    Done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -18,12 +18,25 @@
 
 //Break
 
+// for (let i = 1; i <= 20; i++) {
+//     const element = i;
+//     if(element==5)
+//     {
+//      console.log(`Detected 5`)
+//      break
+//     }
+
+//     console.log(`Value of i is ${i}`)
+// }
+
+//Continue
+
 for (let i = 1; i <= 20; i++) {
     const element = i;
     if(element==5)
     {
      console.log(`Detected 5`)
-     break
+     continue
     }
 
     console.log(`Value of i is ${i}`)
