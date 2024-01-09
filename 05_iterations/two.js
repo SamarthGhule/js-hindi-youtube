35c478a0b257ba5c09ac05d9f8955f1718cb9f66
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-09 08:21:44 +0000

    done

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//while loop
+
+// let index = 0
+// while(index<=10)
+// {
+//  console.log(`Index value = ${index}`)
+//  index += 2
+// }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0
+
+// while(arr<myArray.length)
+// {
+//  console.log(`Value is ${myArray[arr]}`);
+//  arr += 1
+// }
+
+
+//do while loop
+// let index = 11
+
+// do{
+//     console.log(index);
+//     index += 1
+// }while(index<=10);
+
+//Output: 11
