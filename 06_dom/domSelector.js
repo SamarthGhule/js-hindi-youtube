edb7c86bdf9711b3d2b22a01ad500edff22cb23a
Samarth Ghule <92852347+SamarthGhule@users.noreply.github.com>
2024-01-17 08:40:25 +0000

    Done

---
+++ 06_dom/domSelector.js
@@ -0,0 +1,6 @@
+//(29:55 Interesting in video)
+
+// title.innerText //'DOM Learning on Chai aur Code'
+// title.textContent //'DOM Learning on Chai aur Code test text'
+// title.innerHTML //'DOM Learning on Chai aur Code <span style="display:none">test text</span>'
+
